c08433b5ab1dba3b4cf5ef7e8dd956f2dff5a05d
Thomas <thomas@mbp-de-thomas.home>
2022-06-20 18:14:41 +0200

    Allow cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const corsOptions: CorsOptions = { origin: '*' };
+  app.enableCors(corsOptions);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
